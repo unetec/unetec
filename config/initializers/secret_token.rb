bec99ab22d23582b4de2087ee90cfc57aca9c60a
Juan Alejandro Montanez de la Torre <johann2k5@gmail.com>
2015-07-23 13:59:27 -0500

    Creado el proyecto y agregado status al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tecnet::Application.config.secret_token = '9cf14f2ea7eb321f29616ff0cd21c1972d57c3aee5bba8f20db6e9cee31edc89b1ac92fada6e4b302548202718810c40f39a2f7706723819446253408b951092'
